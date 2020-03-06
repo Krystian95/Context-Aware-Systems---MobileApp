717aa58f5a3af75a1f92117d56aae5dc28fa54ad
LLanzarone <30932371+LLanzarone@users.noreply.github.com>
2020-03-06 18:50:11 +0100

    Implementazione notifiche (non funzionanti)
    Implementazione notifiche (non funzionanti)
    Seguendo la guida usata per TechEarthBlog App

---
+++ progettosistemicontextaware/Pods/Target Support Files/Pods-progettosistemicontextaware/Pods-progettosistemicontextaware-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_progettosistemicontextaware : NSObject
+@end
+@implementation PodsDummy_Pods_progettosistemicontextaware
+@end
--- progettosistemicontextaware/progettosistemicontextaware/AppDelegate.swift
+++ progettosistemicontextaware/progettosistemicontextaware/AppDelegate.swift
@@ -8,15 +8,45 @@
 
 import UIKit
 import CoreData
+import UserNotifications
+import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
 
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+       
         // Override point for customization after application launch.
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+            // For iOS 10 data message (sent via FCM
+            //Messaging.messaging().remoteMessageDelegate = self
+            Messaging.messaging().delegate = self
+        } else {
+            let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+              application.registerUserNotificationSettings(settings)
+        }
+         
+        application.registerForRemoteNotifications()
+         
+        FirebaseApp.configure()
+        
         return true
+    }
+    
+    // The callback to handle data message received via FCM for devices running iOS 10 or above.
+    func applicationReceivedRemoteMessage(_ remoteMessage: MessagingRemoteMessage) {
+        print(remoteMessage.appData)
     }
 
     // MARK: UISceneSession Lifecycle
